bfac9dbe8bcdfea52494e8a6aa547116d5170352
master23451 <93018481+master23451@users.noreply.github.com>
2021-10-22 21:42:11 -0500

    repositorio de sistema medico

---
+++ database/migrations/sistema_medico_migraciones/2021_09_22_215703_create_secretaria_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSecretariaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('secretaria', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',100)->nullable();
+            $table->string('apellidos',100)->nullable();
+            $table->string('profile_photo_path',100)->nullable();
+            $table->string('usuario',100)->nullable();
+            $table->string('contra',250)->nullable();
+            $table->string('email',50)->nullable()->unique();
+            $table->string('numero_contacto',10)->nullable();
+            $table->foreign('id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('secretaria');
+    }
+}
