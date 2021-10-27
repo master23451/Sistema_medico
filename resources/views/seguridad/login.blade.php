186258b881f37079d083f1a67ca914c2a98ec34d
master23451 <93018481+master23451@users.noreply.github.com>
2021-10-27 09:33:12 -0500

    repositorio de sistema medico
    Se agrego la pre-visualizacion de imagenes de perfil.
    y el selector de consultorio de doctor al modificar una ficha ya muetra el dato alamacenado.
    
    Se habilito la opcion de status para deshabilitar a los a los trabajadores o empleados.
    
    Se tradujeron los mensajes de validacion al español, se corrigieron varios errores
    
    se agregaron a las tablas de todos los formularios la indicacion de activado o desactivado
    
    Al momento de eliminar a un usuario aparece un mensaje de confirmacion
    
    Se corrigieron los probelmas con las pestaña pso y la crearcion y edicion.
    
    Se agrego al login se le agrego estilos al login

--- resources/views/seguridad/login.blade.php
+++ resources/views/seguridad/login.blade.php
@@ -1,34 +1,38 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-
     <link rel="stylesheet" href="{{ asset('bootstrap-5.1.1/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/styleLogin.css') }}">
 </head>
 <body>
-<div class="container-fluid">
-    <form action="" method="post">
-        @csrf
-        <div class="mb-4">
-            <label for="email">Correo</label>
-            <input name="email" id="email" type="text" autofocus value="{{ old('email') }}" placeholder="Ingrese su correo electronico" class="form-control">
-            @error('email')<span style="color: #d01414"> {{$message}} </span>@enderror
+<div class="container">
+    <div class="row justify-content-center pt-5 mt-5 mr-1">
+        <div class="col-md-6 col-sm-8 col-xl-4 col-lg-4 formulario">
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="form-group text-center pt-3">
+                    <h1 class="text-white">Bienvenido</h1>
+                </div>
+                <div class="form-group mx-sm-4 pt-4">
+                    <input name="email" id="email" type="text" autofocus value="{{ old('email') }}" placeholder="Ingrese su correo electronico" class="form-control" required>
+                    @error('email')<span style="color: #d01414"> {{$message}} </span>@enderror
+                </div>
+                <div class="form-group mx-sm-4 pt-4">
+                    <input name="password" id="password" type="text" placeholder="Ingrese su contraseña" class="form-control" required>
+                    @error('password')<span style="color: #d01414"> {{$message}} </span>@enderror
+                </div>
+                <div class="form-group mx-sm-4 pt-3">
+                    <button  type="submit" class="btn ingresar">Ingresar</button>
+                </div>
+                <div class="form-group mx-sm-4 pt-3">
+                    <input type="checkbox" name="recordar" id="recordar" class="checkbox_input" checked/>
+                    <label for="recordar" class="checkbox_label">Recurda mi sesión</label>
+                </div>
+            </form>
         </div>
-        <div class="mb-4">
-            <label for="password">Contraseña</label>
-            <input name="password" id="password" type="text" placeholder="Ingrese su contraseña" class="form-control">
-            @error('password')<span style="color: #d01414"> {{$message}} </span>@enderror
-        </div>
-        <div class="mb-4">
-            <label for="recordar">
-            <input type="checkbox" name="recordar" id="recordar" class="form-check" value="true"/>
-                Recurda mi sesión <br>
-            </label>
-        </div>
-        <div class="mb-4">
-            <button  type="submit" class="btn btn-success">Ingresar</button>
-        </div>
-    </form>
+    </div>
 </div>
 </body>
 </html>
